7ea28112e29a9bf1e4e8b10bb57e658bf947b2b8
SAM-LEV-T410\awassell <awassell@fcptech.com>
2017-11-09 16:36:08 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+angular.module('mainApp', ['adf', 'adf.widget.clock', 'adf.widget.weather', 'adf.widget.testWidge'])
+    .config(function(dashboardProvider){
+        dashboardProvider
+            .structure('6-6', {
+                rows: [{
+                    columns: [{
+                        styleClass: 'col-md-6'
+                    }, {
+                        styleClass: 'col-md-6'
+                    }]
+                }]
+            });
+    })
