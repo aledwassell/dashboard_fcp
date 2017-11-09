cc9002273285d66dd83c00a56d77f2175ad1c8de
SAM-LEV-T410\awassell <awassell@fcptech.com>
2017-11-09 17:10:14 +0000

    adding package.json

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ angular.module('mainApp', ['adf', 'adf.widget.clock', 'adf.widget.weather', 'adf
             .structure('6-6', {
                 rows: [{
                     columns: [{
-                        styleClass: 'col-md-6'
+                        styleClass: 'col-md-4'
                     }, {
                         styleClass: 'col-md-6'
+                    }, {
+                        styleClass: 'col-md-2'
                     }]
                 }]
             });
